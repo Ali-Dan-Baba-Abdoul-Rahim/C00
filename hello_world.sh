cf176f5f196a541c4b4930e255f0863de3cd35b3
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-08 17:18:44 +0100

    Update hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -1,6 +1,5 @@
-nano hello_world.sh
 #!/bin/bash
-# Ce script affiche "Hello, World!" à l’écran
-echo "hello, world!"
-chmod +x hello_world.sh
-./hello_world.sh
+
+# Ce script affiche "Hello, World!" à l'écran
+
+echo "Hello, World!"
