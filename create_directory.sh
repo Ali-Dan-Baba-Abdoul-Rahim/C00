7023b17e1f8f75713875f5feb947cbe6badc1a92
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-08 17:23:17 +0100

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -1,6 +1,5 @@
-nano create_directory.sh
 #!/bin/bash
+
 # Ce script crée un répertoire nommé test_directory
-mkdir test_directory
-chmod +x create_directory.sh
-./create_directory.sh
+
+mkdir -p test_directory
