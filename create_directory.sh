4c52069fc29ab70ee4e5d290696a49aa3fe1aaaa
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-09 03:51:53 +0100

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# Ce script crée un répertoire nommé test_directory
+# Nom du répertoire à créer
+dir_name="test_directory"
 
-mkdir test_directory
+# Vérifier si le répertoire existe déjà
+if [ -d "$dir_name" ]; then
+    echo "Le répertoire '$dir_name' existe déjà."
+else
+    # Créer le répertoire
+    mkdir "$dir_name"
+    echo "Le répertoire '$dir_name' a été créé."
+fi
