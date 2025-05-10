8501acfe8eb602f7c681d91b8d3c6d96cdbc108d
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-10 05:51:33 +0100

    Update list_files.sh

--- list_files.sh
+++ list_files.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Ce script affiche la liste de tous les fichiers du répertoire courant
+# Afficher la liste des fichiers dans le répertoire courant
+echo "Liste des fichiers dans le répertoire courant :"
 ls -l
+chmod +x list_files.sh
+./list_files.sh
