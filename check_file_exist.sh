437945b265ff0e51e76d75edf6cfe30808f18133
Ali-Dan-Baba-Abdoul-Rahim <aliarahim542@gmail.com>
2025-05-13 17:16:12 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #demander a l utulisateur d entrer le nom du fichier
 read -p  "Entrez le nom du fichier a verifier :"
-if [-e"€sorkho"];then
+if [-f "$sorkho"];then
 echo "le fichier existe."
 else
 echo "le fichier n'existe pas."
